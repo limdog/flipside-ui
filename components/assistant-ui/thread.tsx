b349193fbf91f9bd519e06b1d78e0fc03b0fb625
Simon Farshid <simon.farshid@outlook.com>
2025-05-21 19:01:26 -0700

    add toolfallback

--- components/assistant-ui/thread.tsx
+++ components/assistant-ui/thread.tsx
@@ -21,6 +21,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { MarkdownText } from "@/components/assistant-ui/markdown-text";
 import { TooltipIconButton } from "@/components/assistant-ui/tooltip-icon-button";
+import { ToolFallback } from "./tool-fallback";
 
 export const Thread: FC = () => {
   return (
@@ -202,7 +203,9 @@ const AssistantMessage: FC = () => {
   return (
     <MessagePrimitive.Root className="grid grid-cols-[auto_auto_1fr] grid-rows-[auto_1fr] relative w-full max-w-[var(--thread-max-width)] py-4">
       <div className="text-foreground max-w-[calc(var(--thread-max-width)*0.8)] break-words leading-7 col-span-2 col-start-2 row-start-1 my-1.5">
-        <MessagePrimitive.Content components={{ Text: MarkdownText }} />
+        <MessagePrimitive.Content
+          components={{ Text: MarkdownText, tools: { Fallback: ToolFallback } }}
+        />
       </div>
 
       <AssistantActionBar />
