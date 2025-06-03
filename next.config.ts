2f5bb8cef27bc26c05a8f6f27dde668d4f2321c2
Lucas Imrie <106358117+limdog@users.noreply.github.com>
2025-06-03 16:15:58 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import path from 'path';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      react: path.resolve(__dirname, 'node_modules/react'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
